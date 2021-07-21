18c19904c22e7578cd76878b465fff06fda3366d
M <m@example.com>
2021-07-21 19:47:07 +0200

    Disabled SSL and now exposing port 5000 in docker

--- WebStats/Startup.cs
+++ WebStats/Startup.cs
@@ -50,8 +50,6 @@ namespace WebStats {
                 
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-                app.UseHttpsRedirection();
             }
 
             app.UseStatusCodePagesWithReExecute("/errors/{0}");
